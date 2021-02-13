67f1d8e528fe68ac8b77d9a44d79046189883a53
Mads Akselsen <akselsenmads@gmail.com>
2021-02-13 16:40:56 +0100

    changed layer tile settings

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 window.onload = init;
-function init(){
+
+function init() {
   const map = new ol.Map({
     view: new ol.View({
       center: [0, 0],
-      zoom: 2
+      zoom: 2,
+
+      rotation: 1,
+      enableRotation: true,
+      multiworld: true,
     }),
     layers: [
       new ol.layer.Tile({
-        source: new ol.source.OSM()
-      })
+        source: new ol.source.OSM(),
+        opacity: 1,
+      }),
     ],
-    target: 'js-map'
-  })
+    target: 'js-map',
+  });
 }
-
